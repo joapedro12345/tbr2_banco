82b038f2688438af97252fdc50db9ed4a7002e1c
joapedro12345 <104033078+joapedro12345@users.noreply.github.com>
2023-09-26 21:34:22 -0300

    ex 3

--- listedeexerciciosbanco.sql
+++ listedeexerciciosbanco.sql
@@ -22,3 +22,17 @@ DELIMITER ;
 CALL categoria_dos_livros("Romance");
 CALL categoria_dos_livros("Autoajuda");
 CALL categoria_dos_livros("Ficção Científica");
+
+-- exercício 3 --
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN p_categoria varchar(100))
+BEGIN
+    SELECT Categoria_ID AS ID_DA_CATEGÓRIA , COUNT(*) AS QUANTIDADE_DE_LIVROS FROM Livro 
+    WHERE Categoria_ID = (SELECT Categoria_ID FROM Categoria 
+    WHERE Nome = p_categoria) GROUP BY Categoria_ID ORDER BY COUNT(Categoria_ID);
+END
+//
+
+DELIMITER ;
+drop procedure sp_LivrosPorCategoria;
+CALL sp_LivrosPorCategoria("Autoajuda");
