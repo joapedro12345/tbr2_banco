55a4143d382d771015a0e4fafe0e02483eb34f6d
joapedro12345 <104033078+joapedro12345@users.noreply.github.com>
2023-09-26 21:33:03 -0300

    ex 2

--- listedeexerciciosbanco.sql
+++ listedeexerciciosbanco.sql
@@ -8,3 +8,17 @@ END
 DELIMITER ;
 
 CALL listando_autoures();
+
+-- exercício 2 --
+DELIMITER //
+CREATE PROCEDURE categoria_dos_livros (IN p_categoria varchar(100))
+BEGIN
+    SELECT titulo FROM Livro 
+    WHERE Categoria_ID = (SELECT Categoria_ID FROM Categoria 
+    WHERE Nome = p_categoria);
+END
+//
+DELIMITER ;
+CALL categoria_dos_livros("Romance");
+CALL categoria_dos_livros("Autoajuda");
+CALL categoria_dos_livros("Ficção Científica");
