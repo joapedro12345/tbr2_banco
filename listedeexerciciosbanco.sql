7e5c6e8297d647a383c2b06c58e83d68c87a4007
joapedro12345 <104033078+joapedro12345@users.noreply.github.com>
2023-09-26 21:35:27 -0300

    ex 4

--- listedeexerciciosbanco.sql
+++ listedeexerciciosbanco.sql
@@ -36,3 +36,26 @@ END
 DELIMITER ;
 drop procedure sp_LivrosPorCategoria;
 CALL sp_LivrosPorCategoria("Autoajuda");
+
+-- exercício 4 --
+DELIMITER //
+CREATE PROCEDURE sp_VerificarLivrosCategoria(IN categoria_valor varchar(100), OUT tf_livros varchar(30))
+BEGIN
+    DECLARE verificar INT;
+    WITH Selet_ID as (
+        SELECT (select Categoria_ID from categoria where nome = Categoria_valor) as cate_valor
+    )
+
+
+    SELECT COUNT(*) INTO verificar FROM livro INNER JOIN Selet_ID on cate_valor = Categoria_ID;
+    IF verificar > 0 THEN
+        SET tf_livros = 'Possui Livros';
+    ELSE
+        SET tf_livros = 'Não Possui Livros';
+    END IF;
+END;
+//
+DELIMITER ;
+DROP PROCEDURE  sp_VerificarLivrosCategoria;
+CALL sp_VerificarLivrosCategoria('Ciência', @ver);
+SELECT @ver as tem_ou_não;
